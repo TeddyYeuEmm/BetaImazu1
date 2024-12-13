38f138224f27465c7de660840daddee87d19c789
TeddyYeuEmm <greennart20@gmail.com>
2024-12-14 06:22:33 +0700

    Add files via upload

--- ImazuHub Beta.lua
+++ ImazuHub Beta.lua
@@ -212,7 +212,7 @@ local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nguyen
     local Window = Fluent:CreateWindow({
     Title = "Imazu Hub " ,
     SubTitle = "Beta🫠",
-    TabWidth = 160,
+    TabWidth = 200,
     Size = UDim2.fromOffset(530, 350),
     Acrylic = false, -- The blur may be detectable, setting this to false disables blur entirely
     Theme = "",
@@ -224,6 +224,7 @@ local Tabs = {
     About = Window: AddTab({ Title = "• information", Icon = "clipboard" }),
 	Settings = Window:AddTab({ Title = "• Settings", Icon = "cog" }),
 	Main = Window:AddTab({ Title = "• Auto Farm", Icon = "ghost" }),
+  Item Farm = Window:AddTab({ Title = "• Item", Icon = "sword"})
 }
 
 local Options = Fluent.Options
